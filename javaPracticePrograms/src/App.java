be5127cbd7ee9e797fe48596fa5c254ca715c2e8
Neil Wayne <neilwayne0514@gmail.com>
2025-02-15 17:30:41 +0800

    App program has been added

--- javaPracticePrograms/src/App.java
+++ javaPracticePrograms/src/App.java
@@ -1,5 +1,27 @@
+import java.util.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        // start program
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number you want to add: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Enter another number you want to add: ");
+        int num2 = scanner.nextInt();
+
+        System.out.println(sum(num1, num2));
+    }
+
+    static int sum(int num1, int num2) {
+        if (num2 > num1) {
+            return num2 + sum(num1, num2 - 1);
+        } else if (num1 > num2) {
+            return num1 + sum(num1 - 1, num2);
+        } else {
+            return num1;
+        }
     }
 }
