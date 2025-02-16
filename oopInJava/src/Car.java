b512fb10886d2b12453fd43741ca50be99098800
Neil Wayne <neilwayne0514@gmail.com>
2025-02-17 03:52:53 +0800

    "modified"

--- oopInJava/src/Car.java
+++ oopInJava/src/Car.java
@@ -10,7 +10,7 @@ public class Car {
 
     void display() {
         System.out.println("Make: " + make);
-        System.out.println("Model: " + model);
+        System.out.println("Model: " + model);  
         System.out.println("Year: " + year);
         System.out.println("Price: " + price);
         System.out.println("Is used: " + isUsed);
