0ed1dc8274d270c4c3515ce65b6a1daa528eec22
Neil Wayne <neilwayne0514@gmail.com>
2025-02-16 17:39:52 +0800

    overLoadConstructor program and Basic Command Line and Git Commands has been modified

---
+++ oopInJava/src/User.java
@@ -0,0 +1,6 @@
+// for overLoadConstructor.java
+
+public class User {
+    
+
+}
---
+++ oopInJava/src/overLoadConstructor.java
@@ -0,0 +1,6 @@
+class overLoadConstructor{
+    public static void main(String[] args) {
+        // overloaded constructor = allows a class to have multiple constructors with different parameters list.
+        //                          Enable objects to be initialized with different sets of data.
+    }
+}
