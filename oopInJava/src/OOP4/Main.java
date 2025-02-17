5ca8b756e670e39bba04e07b9a50ce14d93d61dc
Neil Wayne <neilwayne0514@gmail.com>
2025-02-17 20:48:06 +0800

    OOP4 has been added

---
+++ oopInJava/src/OOP4/Main.java
@@ -0,0 +1,20 @@
+package OOP4;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // array of an object = is an array of reference variables of the object
+        //                      (not an array of the object itself)
+        //                      in simple terms, it is an array of pointers to the object
+
+        Car[] cars = {new Car("Toyota", "Red"),
+                      new Car("Honda", "Blue"),
+                      new Car("Ford", "Green")};
+
+        for(Car car: cars){
+            car.color = "White";
+            car.drive();
+        }
+    }
+}
+
