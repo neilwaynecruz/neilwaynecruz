dcd87f40e369b5c356a766302fce529bf210a91e
Neil Wayne <neilwayne0514@gmail.com>
2025-02-17 21:31:16 +0800

    MODIFIED

---
+++ oopInJava/src/OOP5/Friend.java
@@ -0,0 +1,18 @@
+package OOP5;
+
+public class Friend {
+
+    static int numOfFriends;
+    String name;
+    int age;
+
+    Friend(String name, int age){
+        this.name = name;
+        this.age = age;
+        numOfFriends++;
+    }
+
+    static void showFriend(){
+        System.out.println("You have " + numOfFriends + " total friends");
+    }
+}
