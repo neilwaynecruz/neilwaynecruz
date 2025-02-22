e17b95c84b9617a6548813754512025ad2c841ed
Neil Wayne <neilwayne0514@gmail.com>
2025-02-22 17:27:24 +0800

    ADDED

---
+++ prac-act-java/src/REVIEW/Solution.java
@@ -0,0 +1,18 @@
+package REVIEW;
+
+import java.util.*;
+
+class Solution{
+    public static void main(String []argh){
+
+        try{
+            int i = Integer.parseInt("HELLO");
+        }
+        catch (){
+            System.out.println("HELLO BRO, YOU CANT MAKE AN Int to that");
+        }
+
+
+
+    }
+}
