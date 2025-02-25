46345b3a3ef7d82ae941ffd33ed0547c2482b99c
Neil Wayne <neilwayne0514@gmail.com>
2025-02-25 22:05:16 +0800

    MODIFIED

---
+++ prac-act-java/src/REVIEW/BANK/CLASSOOP.java
@@ -0,0 +1,13 @@
+package REVIEW.BANK;
+
+public class CLASSOOP {
+
+    String name;
+    int age;
+
+    public void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+
+}
---
+++ prac-act-java/src/REVIEW/OOPS.java
@@ -0,0 +1,19 @@
+package REVIEW;
+
+import REVIEW.BANK.CLASSOOP;
+import java.io.*;
+
+
+public class OOPS {
+    public static void main(String[] args) {
+
+
+        CLASSOOP oop = new CLASSOOP();
+
+
+
+
+    }
+
+
+}
