935f913c6fb1f667153a8e12f859cf82b237906b
Neil Wayne <neilwayne0514@gmail.com>
2025-03-05 14:48:40 +0800

    ADDED

---
+++ firstPcProgram/firstPcProgram/src/GUI/Frames/Frames.java
@@ -0,0 +1,13 @@
+package GUI.Frames;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Frames {
+    public static void main(String[] args) {
+
+        MyFrame myframe = new MyFrame(); // Create an instance of
+
+    }
+
+}
---
+++ firstPcProgram/firstPcProgram/src/GUI/Frames/MyFrame.java
@@ -0,0 +1,27 @@
+package GUI.Frames;
+                                    //JFrame frame = new JFrame(); // Create a frame
+import javax.swing.*;
+import java.awt.*;
+
+public class MyFrame extends JFrame {
+
+
+    MyFrame(){
+
+        this.setTitle("My First Frame");                       // Set title of frame
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   // exit when you click the x button
+        this.setResizable(false);                          // Prevent frame from being resized
+        this.setSize(500, 500);                // Set size of frame x, y dimensions
+
+        ImageIcon image = new ImageIcon("C:\\mainMasterBranch\\firstPcProgram\\firstPcProgram\\src\\GUI\\Frames\\img.png");
+        // Create an ImageIcon
+        this.setIconImage(image.getImage());               // Change icon of frame
+
+
+        this.getContentPane().setBackground(new Color(29, 85, 141)); // Change color of background
+
+        this.setVisible(true); // Make frame visible
+
+    }
+
+}
