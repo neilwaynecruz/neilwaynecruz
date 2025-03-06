629dc2a69f67a2ec6e24d836e2742d8ae5f69f70
Neil Wayne <neilwayne0514@gmail.com>
2025-03-07 03:49:31 +0800

    MODIFIED

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,29 @@
-print("hello world")
+
+# a = 0
+#
+# for i in range (5,0,-1):
+#     if i <= 4:
+#         i = 4
+#     for j in range (i, a, -1):
+#         print(j, end="")
+#     print()
+#     if i == 4:
+#         a += 1
+
+#
+# import time
+#
+# countdown = int(input("How many seconds you want to count down? "))
+#
+# for i in range(countdown,0,-1):
+#     seconds = i % 60
+#     minutes = int(i / 60) % 60  #remove %60 if you want to have minutes, hours only
+#     hours = int(i / 3600) % 24  #remove %24 if you want to have hours, minutes, seconds only
+#     days = int(i / 86400) % 365 #remove %365 if you want to have days, hours, minutes, seconds only
+#     years = int(i / 31536000)   #remove this line if you want to have days, hours, minutes, seconds only
+#     print(f"{years:02d}:{days:03d}:{hours:02d}:{minutes:02d}:{seconds:02d}")
+#     time.sleep(1)
+# else:
+#     print("GOOD MORNINGGGG!!")
+
+
