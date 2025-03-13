8bda005420410a347a6a9befaa806256ace5663d
Neil Wayne <neilwayne0514@gmail.com>
2025-03-14 00:16:37 +0800

    MODIFIED

--- firstPcProgram/firstPcProgram/src/GUI/GridLayout/Main.java
+++ firstPcProgram/firstPcProgram/src/GUI/GridLayout/Main.java
@@ -1,5 +1,8 @@
 package GUI.GridLayout;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -10,9 +13,28 @@ public class Main {
 //                            and each cell is the same size
 
 
+        JFrame frame = new JFrame();
+
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setTitle("GridLayout");
+        frame.getContentPane().setBackground(Color.darkGray);
+        frame.setSize(800,800);
+        frame.setLayout(new GridLayout(2,2,12,12));
 
 
+        frame.add(new JButton("1"));
+        frame.add(new JButton("2"));
+        frame.add(new JButton("3"));
+        frame.add(new JButton("4"));
+        frame.add(new JButton("5"));
+        frame.add(new JButton("6"));
+        frame.add(new JButton("7"));
+        frame.add(new JButton("8"));
+        frame.add(new JButton("9"));
+        frame.add(new JButton("10"));
 
+
+        frame.setVisible(true);
 
     }
 }
--- firstPcProgram/firstPcProgram/src/GUI/JLayeredPane/Main.java
+++ firstPcProgram/firstPcProgram/src/GUI/JLayeredPane/Main.java
@@ -1,4 +1,50 @@
 package GUI.JLayeredPane;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
+    public static void main(String[] args) {
+
+//        JLayeredPane = Swing container that provides a
+//                       third dimension for positioning components
+
+//                       ex. depth, Z-index
+
+
+        JLabel label1 = new JLabel();
+        label1.setOpaque(true);
+        label1.setBackground(Color.RED);
+        label1.setBounds(150,150,250,250);
+
+        JLabel label2 = new JLabel();
+        label2.setOpaque(true);
+        label2.setBackground(Color.BLUE);
+        label2.setBounds(200,200,250,250);
+
+        JLabel label3 = new JLabel();
+        label3.setOpaque(true);
+        label3.setBackground(Color.MAGENTA);
+        label3.setBounds(250,250,250,250);
+
+
+
+        JLayeredPane layeredPane = new JLayeredPane();
+        layeredPane.setBounds(0,0,800,800);
+        layeredPane.add(label1,Integer.valueOf(1));
+        layeredPane.add(label2,Integer.valueOf(2));
+        layeredPane.add(label3,Integer.valueOf(3));
+
+
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800,800);
+        frame.setTitle("JLayeredPane");
+        frame.setLayout(null);
+
+
+        frame.add(layeredPane);
+        frame.setVisible(true);
+
+    }
 }
