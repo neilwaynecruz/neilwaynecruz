6bf0e67dc694a2e609105eb557d3c5546343d032
Neil Wayne <neilwayne0514@gmail.com>
2025-03-26 18:59:13 +0800

    ADDED

--- firstPcProgram/firstPcProgram/src/GUI/CheckBoxes/Main.java
+++ firstPcProgram/firstPcProgram/src/GUI/CheckBoxes/Main.java
@@ -1,4 +1,7 @@
 package GUI.CheckBoxes;
 
 public class Main {
+    public static void main(String[] args) {
+        new MyFrame();
+    }
 }
---
+++ firstPcProgram/firstPcProgram/src/GUI/CheckBoxes/MyFrame.java
@@ -0,0 +1,58 @@
+package GUI.CheckBoxes;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Objects;
+
+public class MyFrame extends JFrame implements ActionListener {
+
+    JButton button;
+    JCheckBox checkBox;
+    ImageIcon xIcon;
+    ImageIcon checkIcon;
+
+    MyFrame(){
+        this.setTitle("CheckBoxes");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(new FlowLayout());
+
+        xIcon = new ImageIcon(Objects.requireNonNull(getClass().getResource("X.png")));
+        checkIcon = new ImageIcon(Objects.requireNonNull(getClass().getResource("Check.png")));
+
+
+        button = new JButton("Submit");
+        button.addActionListener(this);
+
+        //for JCheckBox
+        checkBox = new JCheckBox();
+        checkBox.setText("POGI BA KO TOL?");
+        checkBox.setFocusable(Boolean.FALSE);
+        checkBox.setFont(new Font("Serif", Font.BOLD, 38));
+        checkBox.setForeground(new Color(171, 67, 67));
+        checkBox.setSelected(true);
+        checkBox.setIcon(xIcon);
+        checkBox.setSelectedIcon(checkIcon);
+
+        this.add(button);
+        this.add(checkBox);
+        this.pack();
+        this.setVisible(true);
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == button){
+
+            if(checkBox.isSelected()){
+                System.out.println("YOU'RE A ROBOT!!");
+            }
+            else{
+                System.out.println("YOU'RE NOT A ROBOT!!");
+            }
+        }
+
+    }
+}
